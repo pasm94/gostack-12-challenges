3b48ff442dc9d11d8316613876e5ce9525d6e7e2
pasm94 <paulomuller_1994@hotmail.com>
2020-08-21 10:03:47 -0300

    add all

---
+++ challenge-reactjs-concepts/src/App.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+import api from './services/api'
+
+import "./styles.css";
+
+function App() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data)
+    })
+  }, [])
+
+  async function handleAddRepository() {
+    const response = await api.post('repositories', {
+      title: 'Novo Projeto',
+      url: "teste.com",
+      techs: ['ReactJS', 'React Native']
+    })
+
+    setRepositories([...repositories, response.data])
+  }
+
+
+  async function handleRemoveRepository(id) {
+    await api.delete(`repositories/${id}`)
+
+    const newRepositories = repositories.filter(
+      repository => repository.id !== id
+    )
+
+    setRepositories(newRepositories);
+
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
+
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
